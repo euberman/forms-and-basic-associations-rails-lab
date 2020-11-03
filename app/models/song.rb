b6ad5da54e0279a5d8e01aced92698f06b858364
euberman <euberman@gmail.com>
2020-11-03 12:32:25 -0600

    commit tree 5ba4c159e277e2fc2b5d171ad3e61bff9f1c6f8c

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,30 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(artist_name)
+    artist = Artist.find_or_create_by(name: artist_name)
+    if artist
+      self.artist = artist
+    else 
+      raise "#artist_name=(artist_name) artist record not found"
+    end
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+         self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
 end
